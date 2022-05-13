7734fca0119de2d5a11b7a4d9cf991b8c7494a33
iwann <ivan.ravinskiy@gmail.com>
2022-05-13 16:43:29 +0300

    refactor login screen

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -1,23 +1,26 @@
-import {Button, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
 import React from 'react';
 import {useDispatch} from 'react-redux';
 import {isAuthProfile} from '../../reducers/fbSlice';
 import {LoginStyles} from './styles';
+import {LoginButton} from 'react-native-fbsdk-next';
 
 export const LoginScreen = () => {
   // const [] = useState(false);
 
   const dispatch = useDispatch();
 
-  const onFacebookButtonPress = () => {
+  const onFacebookButtonPress = async () => {
+    console.log('click');
     dispatch(isAuthProfile());
   };
+
   return (
     <View style={LoginStyles.main}>
       <Text>init</Text>
-      <Button
-        title="Facebook Sign-In"
-        onPress={() => onFacebookButtonPress()}
+      <LoginButton
+        onLoginFinished={onFacebookButtonPress}
+        onLogoutFinished={() => console.log('logout.')}
       />
     </View>
   );
