e066706b5c4d4d6142da50cc8d9834b85e763da4
iwann <ivan.ravinskiy@gmail.com>
2022-05-13 16:55:35 +0300

    add more actions in fb saga worker

--- src/sagas/FbSagaWorker.ts
+++ src/sagas/FbSagaWorker.ts
@@ -1,9 +1,17 @@
-import {call} from 'redux-saga/effects';
+import {call, put} from 'redux-saga/effects';
+import {AccessToken, Profile} from 'react-native-fbsdk-next';
+import {isAuthProfileSuccess, setAvaImg} from '../reducers/fbSlice';
+import FBProfile from 'react-native-fbsdk-next/lib/typescript/src/FBProfile';
 
 export function* FbSagaWorker() {
   try {
-    yield call(console.log, 'FbSagaWorker start');
+    yield call(AccessToken.refreshCurrentAccessTokenAsync);
+    yield call(AccessToken.getCurrentAccessToken);
+    const profile: FBProfile = yield call(Profile.getCurrentProfile);
+    yield call(console.log, profile);
+    yield put(setAvaImg(profile.imageURL));
+    yield put(isAuthProfileSuccess());
   } catch (e) {
-    yield call(console.log, `FbSagaWorker error ${e}`);
+    yield call(console.log, `FbSagaWorker error: ${e}`);
   }
 }
