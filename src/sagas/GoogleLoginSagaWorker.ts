183038d70bd9cd43f64b9395ed172f01166f384a
iwann <ivan.ravinskiy@gmail.com>
2022-05-16 16:10:08 +0300

    refactor google login logout sagas

--- src/sagas/GoogleLoginSagaWorker.ts
+++ src/sagas/GoogleLoginSagaWorker.ts
@@ -1,13 +1,25 @@
 import {call, put} from 'redux-saga/effects';
 import {isAuthProfileSuccess, setAvaImg} from '../reducers/fbSlice';
 import {GoogleSignin, User} from '@react-native-google-signin/google-signin';
+import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
+
+type AuthCredential = FirebaseAuthTypes.AuthCredential;
 
 export function* GoogleLoginSagaWorker() {
+  const signInWithCredential = (credential: AuthCredential) => {
+    return auth().signInWithCredential(credential);
+  };
   try {
-    yield call(GoogleSignin.configure);
+    yield call(GoogleSignin.configure, {
+      webClientId: '1000664849388-hjlfirisvgc9k7habs4idc5es11fr10d',
+    });
     yield call(GoogleSignin.hasPlayServices);
     const userInfo: User = yield call(GoogleSignin.signIn);
-    yield call(console.log, userInfo);
+    const googleCredential: AuthCredential = yield call(
+      auth.GoogleAuthProvider.credential,
+      userInfo.idToken,
+    );
+    yield call(signInWithCredential, googleCredential);
     yield put(setAvaImg(userInfo.user.photo));
     yield put(isAuthProfileSuccess(true));
   } catch (e) {
--- src/sagas/GoogleLogoutSagaWorker.ts
+++ src/sagas/GoogleLogoutSagaWorker.ts
@@ -1,11 +1,12 @@
 import {call, put} from 'redux-saga/effects';
-import {isAuthProfileSuccess} from '../reducers/fbSlice';
+import {isAuthGoogleLogout, isAuthProfileSuccess} from '../reducers/fbSlice';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
 export function* GoogleLogoutSagaWorker() {
   try {
     yield call(console.log, 'GoogleLogoutSagaWorker start');
     yield call(GoogleSignin.signOut);
+    yield put(isAuthGoogleLogout());
     yield put(isAuthProfileSuccess(false));
   } catch (e) {
     yield call(console.log, `GoogleLogoutSagaWorker error: ${e}`);
